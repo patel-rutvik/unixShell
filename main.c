44ba6b546d9c7f34747acd2b40212332bd5f89a1
Rutvik Patel <43623225+patel-rutvik@users.noreply.github.com>
2020-01-14 20:11:46 -0700

    Helper Text Switch Added

--- main.c
+++ main.c
@@ -7,24 +7,45 @@
 #define MAX_LENGTH 20
 #define MAX_PT_ENTRIES 32
 
+bool helperText = true;
+
+void displayJobs()
+{
+    if (helperText) 
+    {
+        printf("Displaying the current jobs: \n");
+    }
+    return;
+}
 
 void startShell(int argc, char *argv[]) 
 {
-    printf("mini shell running...\n");
-    char command[LINE_LENGTH];
+    if (helperText) 
+    {
+        printf("mini shell running...\n");
+    }
+    char commands[LINE_LENGTH];
     bool shellRunning = true;
 
     while (shellRunning) 
     {
-        scanf ("%s", command);
-        printf ("You entered: %s\n", command);
-
-        if (strcmp(command, "exit") == 0)
+        scanf ("%s", commands);
+        if (helperText)
         {
+            printf ("command entered: %s\n", commands);
+        }
+        if (strcmp(commands, "exit") == 0)  // add functionality to strip spaces..
+        {
+            if (helperText)
+            {
+                printf("shell terminated...\n");
+            }
             break;
+        } else if (strcmp(commands, "jobs") == 0) 
+        {
+            displayJobs();
         }
     }
-    
 }
 
 
