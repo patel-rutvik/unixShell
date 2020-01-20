fd6c85289e47cde589c251bc3bdc0f366d60f8d8
Rutvik Patel <43623225+patel-rutvik@users.noreply.github.com>
2020-01-19 19:09:03 -0700

    Refactoring and Read Line added

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #define MAX_ARGS 7
 #define MAX_LENGTH 20
 #define MAX_PT_ENTRIES 32
+#define BUFFER_SIZE 1024
+#define TOKEN_BUFFER_SIZE 64
 
 bool helperText = true;
 int numProcesses = 0;
@@ -28,11 +30,58 @@ void displayJobs()
     return;
 }
 
-void exitCommand() {
+void exitCommand() 
+{
     printf("\nResources used\n");
     printf("User time =    %d seconds\n", userTime);
     printf("Sys  time =    %d seconds\n\n", sysTime);
     return;
+}
+
+char *readLine() 
+{
+    /*
+    int bufferSize = BUFFER_SIZE;
+    int pos = 0;
+    char *buff = malloc(sizeof(char) * bufferSize);
+    int ch;
+
+    while(true) 
+    {
+        ch = getchar();
+
+        if (ch == '\n' || ch == EOF) {
+            return buff[pos] = '\0';
+        } else {
+            buff[pos] = ch;
+        }
+        pos++;
+    }
+    */
+    
+    char *inputLine = NULL;
+    size_t bufferSize = 0;  // getLine allocates buffer size automatically
+    getline(&inputLine, &bufferSize, stdin);
+    return inputLine;
+    
+}
+
+char **splitLine(char *line) 
+{
+    int tokenBufferSize = TOKEN_BUFFER_SIZE;
+    int pos = 0;
+    char **tokens = malloc(tokenBufferSize * sizeof(char*));
+    char **tok;
+    char * delims = " \t\r\n\a";
+
+    tok = strtok(line, delims);
+    while (tok != NULL) {
+        tokens[pos] = tok;
+        pos++;
+        tok = strtok(NULL, delims);
+    }
+    tokens[pos] = NULL;
+    return tokens;    
 }
 
 void startShell(int argc, char *argv[]) 
@@ -41,6 +90,27 @@ void startShell(int argc, char *argv[])
     {
         printf("mini shell running...\n");
     }
+    char *line;
+    char **arguments;
+    bool shellRunning = true;
+
+    while (shellRunning) {
+        printf("SHELL379: ");
+        line = readLine();
+        arguments = splitLine(line);
+        if (strcmp(arguments[0], "exit") == 0)  // add functionality to strip spaces..
+        {
+            // wait until all processes are completed
+
+            // print out total user/system time for each process
+            exitCommand();
+            shellRunning = false;
+        } else if (strcmp(arguments[0], "jobs") == 0) 
+        {
+            displayJobs();
+        }
+    }
+    /*
     char command[LINE_LENGTH];
     bool shellRunning = true;
     int loopCount = 0;
@@ -53,6 +123,7 @@ void startShell(int argc, char *argv[])
                                 };
     int moreCommandsLen = sizeof(moreCommands)/sizeof(moreCommands[0]);
     bool moreArgs = false;
+    
 
     //TODO: find a way to read by LINE not each WORD...
     // if you press enter... another "SHELL379:"" should show up...
@@ -91,6 +162,7 @@ void startShell(int argc, char *argv[])
             printf("times loop ran: %d\n\n", loopCount++);
         }
     }
+    */
 }
 
 
