7af5eafd1ef6be9ccbabb6e453d7e2bdf61b321d
Rutvik Patel <43623225+patel-rutvik@users.noreply.github.com>
2020-02-07 10:44:28 -0700

    Update redirect.c

--- redirect.c
+++ redirect.c
@@ -1,7 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 Name: Rutvik Patel
-ID: 1530012
-CMPUT 379 Assignment 1: Mini Shell
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include "util.h"
 /*
